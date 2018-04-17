023a555be46bcf31e8a8ee17e8252ce33888ca35
Geordie J <geojay@gmail.com>
2018-04-17 18:48:10 +0200

    Update Foundation to release build w/dsyms, hopefully fixes NSAttributedString.attribute() -> Any?

--- rebuild_foundation.sh
+++ rebuild_foundation.sh
@@ -6,6 +6,8 @@
 # Dependencies: swift android toolchain from http://johnholdsworth.com/android_toolchain.tgz
 #
 
+set -e
+
 USER_DIR="$PWD"
 cd "$(dirname $0)"
 export SWIFT_INSTALLATION_PATH="$PWD"
@@ -20,20 +22,20 @@ if [[ "$(uname)" != "Darwin" ]]; then
 fi
 
 if [[ ! -d swift-corelibs-foundation ]]; then
-    git clone http://github.com/SwiftJava/swift-corelibs-foundation &&
+    git clone http://github.com/SwiftJava/swift-corelibs-foundation
     cd swift-corelibs-foundation && git checkout android-toolchain-1.0 && cd -
-fi &&
+fi
 
 	# Build foundation
 	# Remove default foundation implementation and fetch the version with android support
 
-pushd swift-corelibs-foundation &&
+pushd swift-corelibs-foundation
 
-    export PKG_CONFIG_PATH="$SWIFT_INSTALLATION_PATH/pkgconfig" &&
-    rm -rf "$SWIFT_INSTALLATION_PATH/usr/lib/swift/CoreFoundation" &&
+    export PKG_CONFIG_PATH="$SWIFT_INSTALLATION_PATH/pkgconfig"
+    rm -rf "$SWIFT_INSTALLATION_PATH/usr/lib/swift/CoreFoundation"
 
-    export CLANG="$(dirname $(readlink $SWIFT_INSTALLATION_PATH/usr/bin/swift))/clang" &&
-    export BUILD_DIR="$SWIFT_ANDROID_BUILDPATH/foundation-macosx-x86_64" &&
+    export CLANG="$(dirname $(readlink $SWIFT_INSTALLATION_PATH/usr/bin/swift))/clang"
+    export BUILD_DIR="$SWIFT_ANDROID_BUILDPATH/foundation-macosx-x86_64"
 
     env \
         SWIFTC="$SWIFT_INSTALLATION_PATH/usr/Darwin/swiftc" \
@@ -41,31 +43,29 @@ pushd swift-corelibs-foundation &&
         SDKROOT="$SWIFT_ANDROID_BUILDPATH/swift-macosx-x86_64" \
         DSTROOT="/" \
         PREFIX="/usr/" \
-        CFLAGS="-DDEPLOYMENT_TARGET_ANDROID -DDEPLOYMENT_ENABLE_LIBDISPATCH --sysroot=$SWIFT_INSTALLATION_PATH/ndk-android-21 -I${SDKROOT}/lib/swift -I$SWIFT_INSTALLATION_PATH/ndk-android-21/support/include -I$PWD/closure" \
-        SWIFTCFLAGS="-DDEPLOYMENT_TARGET_ANDROID -DDEPLOYMENT_ENABLE_LIBDISPATCH" \
+        CFLAGS="-DDEPLOYMENT_TARGET_ANDROID -DDEPLOYMENT_ENABLE_LIBDISPATCH --sysroot=$SWIFT_INSTALLATION_PATH/ndk-android-21 -I${SDKROOT}/lib/swift -I$SWIFT_INSTALLATION_PATH/ndk-android-21/support/include -I$PWD/closure -g" \
+        SWIFTCFLAGS="-DDEPLOYMENT_TARGET_ANDROID -DDEPLOYMENT_ENABLE_LIBDISPATCH -g" \
         LDFLAGS="-fuse-ld=gold --sysroot=$SWIFT_INSTALLATION_PATH/ndk-android-21 -L$SWIFT_INSTALLATION_PATH/usr/Darwin -ldispatch " \
         SDKROOT=$SWIFT_INSTALLATION_PATH/usr \
         ./configure \
-            Debug \
+            Release \
             --target=armv7-none-linux-androideabi \
             --sysroot=$SWIFT_INSTALLATION_PATH/ndk-android-21 \
             -DXCTEST_BUILD_DIR=$SWIFT_ANDROID_BUILDPATH/xctest-linux-x86_64 \
             -DLIBDISPATCH_SOURCE_DIR=$SWIFT_INSTALLATION_PATH/usr/lib/swift \
-            -DLIBDISPATCH_BUILD_DIR=$SWIFT_INSTALLATION_PATH/usr/lib/swift &&
+            -DLIBDISPATCH_BUILD_DIR=$SWIFT_INSTALLATION_PATH/usr/lib/swift
 
     # Prepend SYSROOT env variable to ninja.build script
     # SYSROOT is not being passed from build.py / script.py to the ninja file yet
-    echo "SYSROOT=$SYSROOT" > build.ninja.new &&
-    cat build.ninja >> build.ninja.new &&
-    mv -f build.ninja.new build.ninja &&
+    echo "SYSROOT=$SYSROOT" > build.ninja.new
+    cat build.ninja >> build.ninja.new
+    mv -f build.ninja.new build.ninja
 
-    ninja &&
+    ninja
 			
     # There's no installation script for foundation yet, so the installation needs to be done manually.
     # Apparently the installation for the main script is in swift repo.
-    rsync -a $BUILD_DIR/Foundation/usr/lib/swift/CoreFoundation $SWIFT_INSTALLATION_PATH/usr/lib/swift &&
-    cp -v $BUILD_DIR/Foundation/libFoundation.so $SWIFT_INSTALLATION_PATH/usr/lib/swift/android/ &&
-    cp -v $BUILD_DIR/Foundation/Foundation.swift* $SWIFT_INSTALLATION_PATH/usr/lib/swift/android/armv7/ &&
-
+    rsync -a $BUILD_DIR/Foundation/usr/lib/swift/CoreFoundation $SWIFT_INSTALLATION_PATH/usr/lib/swift
+    cp -v $BUILD_DIR/Foundation/libFoundation.so $SWIFT_INSTALLATION_PATH/usr/lib/swift/android/
+    cp -v $BUILD_DIR/Foundation/Foundation.swift* $SWIFT_INSTALLATION_PATH/usr/lib/swift/android/armv7/
 popd
-
