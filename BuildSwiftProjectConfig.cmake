4bca179cac4c31d808fc6e20eb983f964c67aeb1
michaelknoch <info@michaelknoch.de>
2020-01-10 15:23:21 +0100

    even more cleanup

--- BuildSwiftProjectConfig.cmake
+++ BuildSwiftProjectConfig.cmake
@@ -11,16 +11,24 @@ file(WRITE .empty.c "") # we need to build *something*, even if it's an empty fi
 add_library(swiftBuildDummy SHARED .empty.c)
 add_dependencies(swiftBuildDummy allTarget)
 
+set(LIBRARY_OUTPUT_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/src/main/jniLibs")
+
+# Path to a file which will never be built by cmake and therefore never exists.
+# Referencing this file as dependency in `allTarget` ensures `swift-build.sh`
+# will always be invoked when building via gradle.
+# If there is actually nothing to do `swift-build.sh` will exit immediately.
+set(NEVER_EXISTING_OUTPUT ./nop)
+
 function(build_swift_project)
     add_custom_command(
-        OUTPUT always_rebuild
+        OUTPUT NEVER_EXISTING_OUTPUT
         COMMAND LIBRARY_OUTPUT_DIRECTORY=${LIBRARY_OUTPUT_DIRECTORY} ANDROID_ABI=${ANDROID_ABI} CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} "${SWIFT_TOOLCHAIN_ROOT}/swift-build.sh" ${PROJECT_DIRECTORY}
         WORKING_DIRECTORY ${PROJECT_DIRECTORY}
         VERBATIM
     )
 
     add_custom_target(allTarget ALL
-        DEPENDS always_rebuild
+        DEPENDS NEVER_EXISTING_OUTPUT
         VERBATIM
     )
 endfunction(build_swift_project)
