0dee6c6615c0d8fbf03a9470504d335ec3c95314
michaelknoch <info@michaelknoch.de>
2020-01-13 21:37:07 +0100

    download artifacts from s3

--- setup.sh
+++ setup.sh
@@ -2,22 +2,9 @@
 
 set -e
 
-ORIGINAL_PWD=$PWD
 SCRIPT_ROOT=$(cd "$(dirname "$0")"; pwd -P)
-AZURE_BASE_PATH=https://dev.azure.com/compnerd/swift-build
 PATH_TO_SWIFT_TOOLCHAIN="$SCRIPT_ROOT/swift-flowkey.xctoolchain"
-
-SDK_BUILD_ID=24083
-ICU_BUILD_ID=24100
-
-if [[ `uname` == 'Darwin' ]]; then
-    TOOLCHAIN_BUILD_ID=24080
-elif [[ `uname` == 'Linux' ]]; then
-    TOOLCHAIN_BUILD_ID=24081
-else
-    log "unsupported architecture"
-    exit 1
-fi
+UNAME=`uname`
 
 log() {
     echo "[swift-android-toolchain] $*"
@@ -25,79 +12,35 @@ log() {
 
 if [[ $1 = "--clean" ]]; then
     log "Let's start from scratch ..."
-    git -C $SCRIPT_ROOT clean -xdf
+    clean
 fi
+
+clean() {
+    git -C $SCRIPT_ROOT clean -xdf
+}
 
 rm -rf $SCRIPT_ROOT/temp
 mkdir -p $SCRIPT_ROOT/temp
 cd $SCRIPT_ROOT/temp
 
-downloadToolchain() {
-    if [[ `uname` == 'Darwin' ]]; then
-        BUILD_DEFINITIONS=15
-    elif [[ `uname` == 'Linux' ]]; then
-        BUILD_DEFINITIONS=14
-    else
-        log "unsupported architecture"
-        exit 1
-    fi
+downloadArtifacts() {
+    log "Downloading Toolchain Artifacts..."
 
-    TOOLCHAIN_BUILD_ID="${TOOLCHAIN_BUILD_ID:-`curl -s "$AZURE_BASE_PATH/_apis/build/builds?definitions=$BUILD_DEFINITIONS"'&resultFilter=succeeded&$top=1&api-version-string=5.0' | jq ".value[0].id"`}"
-    TOOLCHAIN_ARTIFACT_URL=`curl -s "$AZURE_BASE_PATH/_apis/build/builds/$TOOLCHAIN_BUILD_ID/artifacts?apiversion-string=2.0" | jq -r ".value[0].resource.downloadUrl"`
+    mkdir -p $SCRIPT_ROOT/temp
+    cd $SCRIPT_ROOT/temp
 
-    rm -rf $PATH_TO_SWIFT_TOOLCHAIN
+    BASEPATH="https://swift-toolchain-artifacts.flowkeycdn.com"
+    VERSION="20200112.1"
 
-    log "Downloading custom Swift Toolchain ($TOOLCHAIN_BUILD_ID) ..."
-    curl -J -o ./toolchain.zip `toCachedUrl $TOOLCHAIN_ARTIFACT_URL`
-    log "Finished downloading Toolchain"
-    unzip -qq 'toolchain.zip'
+    curl -JO ${BASEPATH}/${VERSION}/${UNAME}.zip
+    unzip -qq $SCRIPT_ROOT/temp/${UNAME}.zip
 
-    log "Setting up custom Swift Toolchain ..."
-    mv $SCRIPT_ROOT/temp/toolchain-*/Library/Developer/Toolchains/*.xctoolchain $PATH_TO_SWIFT_TOOLCHAIN
-    chmod -R +x $PATH_TO_SWIFT_TOOLCHAIN/usr/bin
-}
+    mv $SCRIPT_ROOT/temp/${UNAME}/swift-flowkey.xctoolchain $SCRIPT_ROOT
+    mv $SCRIPT_ROOT/temp/${UNAME}/Android.sdk-* $SCRIPT_ROOT
 
-toCachedUrl() {
-    # download speed on dev.azure.com is super slow, download from our cache for faster downloads
-    echo -n $1 | sed -e s~https://dev.azure.com~http://swift-ci.flowkey.com~g
-}
-
-downloadAndroidSdks() {
-    SDK_BUILD_ID="${SDK_BUILD_ID:-`curl -s "$AZURE_BASE_PATH"'/_apis/build/builds?definitions=43&resultFilter=succeeded&$top=1&api-version-string=5.0' | jq ".value[0].id"`}"
-    SDK_ARTIFACT_URLS=`curl -s "$AZURE_BASE_PATH/_apis/build/builds/$SDK_BUILD_ID/artifacts?apiversion-string=2.0" | jq ".value" | jq "map_values(.resource.downloadUrl)"`
-
-    log "Downloading Android SDKs ($SDK_BUILD_ID) ..."
-    for URL in $(echo $SDK_ARTIFACT_URLS | jq -r ".[]"); do
-        curl -OJs `toCachedUrl $URL` &
-    done
-    wait
-
-    unzip -qq 'sdk-android-*.zip'
-
-    rm -rf $SCRIPT_ROOT/Android.sdk-*/
-    mv $SCRIPT_ROOT/temp/sdk-android-arm/Library/Developer/Platforms/Android.platform/Developer/SDKs/Android.sdk/ $SCRIPT_ROOT/Android.sdk-armeabi-v7a
-    mv $SCRIPT_ROOT/temp/sdk-android-arm64/Library/Developer/Platforms/Android.platform/Developer/SDKs/Android.sdk/ $SCRIPT_ROOT/Android.sdk-arm64-v8a
-    mv $SCRIPT_ROOT/temp/sdk-android-x64/Library/Developer/Platforms/Android.platform/Developer/SDKs/Android.sdk/ $SCRIPT_ROOT/Android.sdk-x86_64
-
-    log "Downloading SDKs finished"
-}
-
-downloadLibs() {
-    ICU_BUILD_ID="${ICU_BUILD_ID:-`curl -s "$AZURE_BASE_PATH"'/_apis/build/builds?definitions=9&resultFilter=succeeded&api-version-string=5.0' | jq ".value[0].id"`}"
-    ICU_ARTIFACTS=`curl -s "$AZURE_BASE_PATH/_apis/build/builds/$ICU_BUILD_ID/artifacts?apiversion-string=2.0" | jq ".value" | jq "map_values(.resource.downloadUrl)"`
-
-    log "Downloading ICU ($ICU_BUILD_ID) ..."
-    for URL in $(echo $ICU_ARTIFACTS | jq -r ".[]"); do
-        # download ICU for android only
-        curl -OJs `toCachedUrl $URL` &
-    done
-    wait
-
-    unzip -qq 'icu-android-*.zip'
-
-    mv $SCRIPT_ROOT/temp/icu-android-arm/Library/icu-64/usr/lib/*.so $SCRIPT_ROOT/libs/armeabi-v7a
-    mv $SCRIPT_ROOT/temp/icu-android-arm64/Library/icu-64/usr/lib/*.so $SCRIPT_ROOT/libs/arm64-v8a
-    mv $SCRIPT_ROOT/temp/icu-android-x64/Library/icu-64/usr/lib/*.so $SCRIPT_ROOT/libs/x86_64
+    mv $SCRIPT_ROOT/temp/${UNAME}/libs/armeabi-v7a/*.so $SCRIPT_ROOT/libs/armeabi-v7a
+    mv $SCRIPT_ROOT/temp/${UNAME}/libs/arm64-v8a/*.so $SCRIPT_ROOT/libs/arm64-v8a
+    mv $SCRIPT_ROOT/temp/${UNAME}/libs/x86_64/*.so $SCRIPT_ROOT/libs/x86_64
 }
 
 setup() {
@@ -126,19 +69,11 @@ setup() {
     log "Setup finished"
 }
 
-if [[ ! -d $PATH_TO_SWIFT_TOOLCHAIN ]]; then
-    downloadToolchain
-fi
-
-if [[ ! -d $SCRIPT_ROOT/Android.sdk-armeabi-v7a ]]; then
-    downloadAndroidSdks
-fi
-
-if [[ ! -f $SCRIPT_ROOT/libs/armeabi-v7a/libicuuc64.so ]]; then
-    downloadLibs
+if [[ ! -d $PATH_TO_SWIFT_TOOLCHAIN ]] || [[ ! -d $SCRIPT_ROOT/Android.sdk-armeabi-v7a ]] || [[ ! -f $SCRIPT_ROOT/libs/armeabi-v7a/libicuuc64.so ]]; then
+    clean
+    downloadArtifacts
 fi
 
 setup
 
-cd $ORIGINAL_PWD
 rm -rf $SCRIPT_ROOT/temp
