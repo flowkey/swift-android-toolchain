f48ce5a5622c97e4f8ddec74ac52897b810fc0b0
Michael Knoch <info@michaelknoch.de>
2020-01-20 23:10:47 +0100

    fix typo in setup.sh error message (#7)

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ if [[ ! ${ANDROID_NDK_PATH} ]]; then
     if [[ ${UNAME} == "Darwin" ]]; then
         log "Download from https://dl.google.com/android/repository/android-ndk-r16b-darwin-x86_64.zip"
     elif [[ ${UNAME} == "Linux" ]]; then
-        log "Download from https://dl.google.com/android/repository/android-ndk-r16b-linux-x86_64.zipp"
+        log "Download from https://dl.google.com/android/repository/android-ndk-r16b-linux-x86_64.zip"
     fi
     exit 1
 fi
