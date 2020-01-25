1f6bb53f1bb13b5b2459f43555bbf978c0f2c4a0
Michael Knoch <info@michaelknoch.de>
2020-01-25 17:32:15 +0100

    wip

--- swift-build.sh
+++ swift-build.sh
@@ -32,21 +32,21 @@ build() {
     # You need a different SDK per arch, e.g. swift-android-toolchain/Android.sdk-armeabi-v7a/
     export ANDROID_SDK="$SWIFT_ANDROID_TOOLCHAIN_PATH/Android.sdk-${ANDROID_ABI}"
 
-    $SWIFT_ANDROID_TOOLCHAIN_PATH/setup.sh
+    # $SWIFT_ANDROID_TOOLCHAIN_PATH/setup.sh
 
-    cmake \
-        -G Ninja \
-        -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
-        -DANDROID_ABI=${ANDROID_ABI} \
-        -DANDROID_PLATFORM=android-21 \
-        -DANDROID_NDK="${ANDROID_NDK_PATH}" \
-        -DSWIFT_SDK="${ANDROID_SDK}" \
-        -DCMAKE_TOOLCHAIN_FILE="${ANDROID_NDK_PATH}/build/cmake/android.toolchain.cmake" \
-        -C "${SWIFT_ANDROID_TOOLCHAIN_PATH}/cmake_caches.cmake" \
-        -DCMAKE_Swift_COMPILER="${ANDROID_SDK}/usr/bin/swiftc" \
-        -DCMAKE_Swift_COMPILER_FORCED=TRUE \
-        -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=${LIBRARY_OUTPUT_DIRECTORY} \
-        ${PROJECT_DIRECTORY}
+    # cmake \
+    #     -G Ninja \
+    #     -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
+    #     -DANDROID_ABI=${ANDROID_ABI} \
+    #     -DANDROID_PLATFORM=android-21 \
+    #     -DANDROID_NDK="${ANDROID_NDK_PATH}" \
+    #     -DSWIFT_SDK="${ANDROID_SDK}" \
+    #     -DCMAKE_TOOLCHAIN_FILE="${ANDROID_NDK_PATH}/build/cmake/android.toolchain.cmake" \
+    #     -C "${SWIFT_ANDROID_TOOLCHAIN_PATH}/cmake_caches.cmake" \
+    #     -DCMAKE_Swift_COMPILER="${ANDROID_SDK}/usr/bin/swiftc" \
+    #     -DCMAKE_Swift_COMPILER_FORCED=TRUE \
+    #     -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=${LIBRARY_OUTPUT_DIRECTORY} \
+    #     ${PROJECT_DIRECTORY}
 
     cmake --build . #--verbose
 
