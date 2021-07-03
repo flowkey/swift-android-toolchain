570a00a7953d83ccee47902e381b84f367063288
Michael Knoch <info@michaelknoch.de>
2021-07-04 01:14:21 +0200

    fix path to toolchain

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ downloadToolchain() {
     cd $SCRIPT_ROOT/temp
 
     log "Downloading Toolchain..."
-    curl -LO https://swift-toolchain-artifacts.flowkeycdn.com/swift-android-5.3.3.tar.gz
+    curl -LO https://github.com/vgorloff/swift-everywhere-toolchain/releases/download/1.0.68/swift-android-toolchain.tar.gz
     log "Extracting Toolchain..."
     tar -xzf $SCRIPT_ROOT/temp/*.tar.gz
     mv $SCRIPT_ROOT/temp/swift-android-toolchain $SCRIPT_ROOT/swift-android.xctoolchain
