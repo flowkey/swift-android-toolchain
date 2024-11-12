6358c10fdb18a5c8c7f89eb6fbe05aeacc32be4a
Geordie Jay <geojay@gmail.com>
2024-11-12 23:32:58 +0100

    Update for use with 6.0.2 SDK+toolchain via SwiftPM

--- setup.sh
+++ setup.sh
@@ -2,6 +2,25 @@ log() {
     echo "[swift-android-toolchain] $*"
 }
 
+readonly SWIFT_VERSION="6.0.2"
+readonly TOOLCHAIN_PATH="${TOOLCHAIN_PATH:-/Library/Developer/Toolchains/swift-${SWIFT_VERSION}-RELEASE.xctoolchain}"
+if [ ! -d ${TOOLCHAIN_PATH} ]
+then
+    log "Please install the swift-${SWIFT_VERSION}-RELEASE toolchain (or set TOOLCHAIN_PATH)"
+    log "On Mac: https://download.swift.org/swift-${SWIFT_VERSION}-release/xcode/swift-${SWIFT_VERSION}-RELEASE/swift-6.0.2-RELEASE-osx.pkg"
+
+    exit 1
+fi
+
+readonly SWIFT_ANDROID_SDK="swift-${SWIFT_VERSION}-RELEASE-android-24-0.1"
+readonly SWIFT_ANDROID_SDK_CHECKSUM="d75615eac3e614131133c7cc2076b0b8fb4327d89dce802c25cd53e75e1881f4"
+if [ ! $(swift sdk list | grep ${SWIFT_ANDROID_SDK}) ]
+then
+    swift sdk install \
+        https://github.com/finagolfin/swift-android-sdk/releases/download/${SWIFT_VERSION}/${SWIFT_ANDROID_SDK}.artifactbundle.tar.gz \
+        --checksum ${SWIFT_ANDROID_SDK_CHECKSUM}
+fi
+
 readonly NDK_VERSION="27.1.12297006"
 readonly ANDROID_NDK_PATH="${ANDROID_NDK_PATH:-/usr/local/ndk/${NDK_VERSION}}"
 if [[ ! `cat "${ANDROID_NDK_PATH}/CHANGELOG.md" 2> /dev/null` ]]; then
@@ -16,26 +35,19 @@ then
     ANDROID_ABI=arm64-v8a
 fi
 
-: "${SCRIPT_ROOT:=$(cd "$(dirname "$0")"; pwd -P)}"
-
-readonly SDK_DIR="${SCRIPT_ROOT}/sdk"
-readonly TOOLCHAIN_PATH="${TOOLCHAIN_PATH:-/Library/Developer/Toolchains/swift-6.0.1-RELEASE.xctoolchain}"
-
 for LAST_ARGUMENT in $@; do :; done
 readonly PROJECT_DIRECTORY=${LAST_ARGUMENT:-$PWD}
 readonly BUILD_DIR="${PROJECT_DIRECTORY}/build/${ANDROID_ABI}"
 readonly LIBRARY_OUTPUT_DIRECTORY="${LIBRARY_OUTPUT_DIRECTORY:-${PROJECT_DIRECTORY}/libs/${ANDROID_ABI}}"
 readonly CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-"Debug"}
 
-readonly SWIFT_SDK_PATH="${SCRIPT_ROOT}/sdk/swift-6.0.1-RELEASE-android-24-0.1.artifactbundle/swift-6.0.1-release-android-24-sdk/android-27b-sysroot"
-
-readonly HOST=darwin-x86_64 # TODO: add more platforms
+readonly SWIFT_SDK_PATH="${HOME}/Library/org.swift.swiftpm/swift-sdks/${SWIFT_ANDROID_SDK}.artifactbundle/swift-${SWIFT_VERSION}-release-android-24-sdk/android-27c-sysroot"
 
 copySwiftDependencyLibs() {
     log "Copying Swift dependencies..."
     function copyLib {
         local DESTINATION="${LIBRARY_OUTPUT_DIRECTORY}/`basename "$1"`"
-        log "${DESTINATION}"
+        # log "${DESTINATION}"
         if [ "$1" -nt "${DESTINATION}" ]
         then
             mkdir -p "${LIBRARY_OUTPUT_DIRECTORY}"
@@ -54,6 +66,14 @@ copySwiftDependencyLibs() {
     local LIB_FILES=`find "${SWIFT_SDK_PATH}/usr/lib/${TARGET_LIB_DIR}/24" -type f -iname "*.so" -print`
 
     # EXCLUDED_LIBS are optionally provided to script, e.g. from Gradle:
+    # Check if EXCLUDED_LIBS is set; if not, initialize it as an empty array.
+    EXCLUDED_LIBS="${EXCLUDED_LIBS:-}"
+
+    # Append libc++.so to EXCLUDED_LIBS if it’s not already included.
+    if [[ ! " ${EXCLUDED_LIBS[@]} " =~ "libc++.so" ]]; then
+        EXCLUDED_LIBS+=" libc++.so"
+    fi
+
     if [ ${#EXCLUDED_LIBS} != "0" ]
     then
         local EXCLUSIONS_STRING=`for EXCLUSION in ${EXCLUDED_LIBS}; do printf %s "-e ${EXCLUSION} "; done`
@@ -64,56 +84,4 @@ copySwiftDependencyLibs() {
     do
         copyLib "${FILE_PATH}"
     done
-
-    copyLib "${ANDROID_NDK_PATH}/sources/cxx-stl/llvm-libc++/libs/${ANDROID_ABI}/libc++_shared.so"
 }
-
-
-clean() {
-    rm -rf ${SDK_DIR}
-}
-
-if [[ $1 = "--clean" ]]; then
-    log "Let's start from scratch..."
-    clean
-fi
-
-if [ ! -d ${TOOLCHAIN_PATH} ]
-then
-    log "Please install the swift-6.0.1-RELEASE toolchain (or set TOOLCHAIN_PATH)"
-    log "On Mac: https://download.swift.org/swift-6.0.1-release/xcode/swift-6.0.1-RELEASE/swift-6.0.1-RELEASE-osx.pkg"
-    
-    exit 1
-fi
-
-if [[ ! -f "${TOOLCHAIN_PATH}/usr/bin/swift-autolink-extract" ]];
-then
-    log "Missing symlink '${TOOLCHAIN_PATH}/usr/bin/swift-autolink-extract'."
-    log "We need 'sudo' permission to create it (just this once)."
-    sudo ln -s swift ${TOOLCHAIN_PATH}/usr/bin/swift-autolink-extract || exit 1
-fi
-
-downloadSdks() {
-    [ ! -d ${SDK_DIR} ] && mkdir -p ${SDK_DIR}
-    pushd ${SDK_DIR} > /dev/null
-
-    local ORIGINAL_FILENAME="swift-6.0.1-RELEASE-android-24-0.1.artifactbundle.tar.gz"
-
-    if [ ! -f "${ORIGINAL_FILENAME}" ]
-    then
-        log "Downloading ${SWIFT_SDK_PATH} SDK..."
-        local SDK_URL_BASEPATH="https://github.com/finagolfin/swift-android-sdk/releases/download/6.0.1"
-        local SDK_DOWNLOAD_URL="${SDK_URL_BASEPATH}/${ORIGINAL_FILENAME}"
-        curl -LO ${SDK_DOWNLOAD_URL}
-    fi
-
-    if [ ! -d "${SWIFT_SDK_PATH}" ]
-    then
-        log "Extracting ${SWIFT_SDK_PATH} SDK..."
-        tar --extract --file ${ORIGINAL_FILENAME}
-    fi
-
-    popd > /dev/null
-}
-
-downloadSdks
--- swiftpm.sh
+++ swiftpm.sh
@@ -5,22 +5,8 @@ set -e
 readonly SCRIPT_ROOT=$(cd $(dirname $0); echo -n $PWD) # path of this file
 source "${SCRIPT_ROOT}/setup.sh"
 
-readonly BUILD_TYPE=${BUILD_TYPE:-release}
 readonly SCRATCH_PATH="swiftpm-build"
-
-if [ -f ${SCRATCH_PATH}/${ANDROID_ABI}-${BUILD_TYPE}.yaml ]
-then
-    cp -f ${SCRATCH_PATH}/${ANDROID_ABI}-${BUILD_TYPE}.yaml ${SCRATCH_PATH}/${BUILD_TYPE}.yaml
-fi
-
-for arg in "$@"
-do
-    case $arg in
-        --static-swift-stdlib)
-            STATIC_SWIFT_STDLIB=1
-        ;;
-    esac
-done
+readonly BUILD_TYPE=${BUILD_TYPE:-release}
 
 if [ ${ANDROID_ABI} = "armeabi-v7a" ]; then
     TARGET_TRIPLE="armv7-unknown-linux-androideabi24"
@@ -30,67 +16,35 @@ else # assume arm64
     TARGET_TRIPLE="aarch64-unknown-linux-android24"
 fi
 
-if [ "${STATIC_SWIFT_STDLIB}" ]
+if [ -f ${SCRATCH_PATH}/${ANDROID_ABI}-${BUILD_TYPE}.yaml ]
 then
-    readonly DESTINATION_FILE="${SCRIPT_ROOT}/${ANDROID_ABI}_static.json"
-else
-    readonly DESTINATION_FILE="${SCRIPT_ROOT}/${ANDROID_ABI}.json"
+    cp -f ${SCRATCH_PATH}/${ANDROID_ABI}-${BUILD_TYPE}.yaml ${SCRATCH_PATH}/${BUILD_TYPE}.yaml
 fi
 
-if [ ! -f "${DESTINATION_FILE}" ]; then
-    if [ "${STATIC_SWIFT_STDLIB}" ]
-    then
-cat <<- EOF > "${DESTINATION_FILE}"
-{
-    "version": 1,
-    "target": "${TARGET_TRIPLE}",
-    "toolchain-bin-dir": "${TOOLCHAIN_PATH}/usr/bin",
-    "sdk": "${ANDROID_NDK_PATH}/toolchains/llvm/prebuilt/${HOST}/sysroot",
-    "extra-cc-flags": ["-fPIC", "-DSTATIC_SWIFT_STDLIB"],
-    "extra-swiftc-flags": [
-        "-DSTATIC_SWIFT_STDLIB",
-        "-resource-dir",
-        "${SCRIPT_ROOT}/sdk/${ANDROID_ABI}/usr/lib/swift_static",
-        "-tools-directory",
-        "${ANDROID_NDK_PATH}/toolchains/llvm/prebuilt/${HOST}/bin"
-    ],
-    "extra-cpp-flags": ["-lstdc++"]
-}
-EOF
-    else
-cat <<- EOF > "${DESTINATION_FILE}"
-{
-    "version": 1,
-    "target": "${TARGET_TRIPLE}",
-    "toolchain-bin-dir": "${TOOLCHAIN_PATH}/usr/bin",
-    "sdk": "${ANDROID_NDK_PATH}/toolchains/llvm/prebuilt/${HOST}/sysroot",
-    "extra-cc-flags": ["-fPIC"],
-    "extra-swiftc-flags": [
-        "-resource-dir",
-        "${SCRIPT_ROOT}/sdk/${ANDROID_ABI}/usr/lib/swift",
-        "-tools-directory",
-        "${ANDROID_NDK_PATH}/toolchains/llvm/prebuilt/${HOST}/bin"
-    ],
-    "extra-cpp-flags": ["-lstdc++"]
-}
-EOF
-    fi
+if [ -f ${SCRATCH_PATH}/${ANDROID_ABI}-${BUILD_TYPE}.db ]
+then
+    cp -f ${SCRATCH_PATH}/${ANDROID_ABI}-${BUILD_TYPE}.db ${SCRATCH_PATH}/build.db
 fi
 
-# --disable-index-store works around compiler crash in swift 5.7 toolchain:
-# adding a --scratch-path per ABI makes rebuilding faster
-${TOOLCHAIN_PATH}/usr/bin/swift build \
-    --disable-index-store \
-    --scratch-path ${SCRATCH_PATH} \
-    --destination "${DESTINATION_FILE}" \
-    -c ${BUILD_TYPE} \
-    $@
+function swiftBuild {
+    ${TOOLCHAIN_PATH}/usr/bin/swift build \
+        --swift-sdk ${TARGET_TRIPLE} \
+        --toolchain ${TOOLCHAIN_PATH} \
+        --scratch-path ${SCRATCH_PATH} \
+        -c ${BUILD_TYPE} \
+        -Xcc -fPIC \
+        $@
+}
 
+swiftBuild $@
+
+# Speed up subsequent incremental builds
 cp -f ${SCRATCH_PATH}/${BUILD_TYPE}.yaml ${SCRATCH_PATH}/${ANDROID_ABI}-${BUILD_TYPE}.yaml
+cp -f ${SCRATCH_PATH}/build.db ${SCRATCH_PATH}/${ANDROID_ABI}-${BUILD_TYPE}.db
 
 if [ "$LIBRARY_OUTPUT_DIRECTORY" ]
 then
     copySwiftDependencyLibs
     mkdir -p ${LIBRARY_OUTPUT_DIRECTORY}
-    cp -f "${SCRATCH_PATH}/${BUILD_TYPE}"/*.so "${LIBRARY_OUTPUT_DIRECTORY}"
+    cp -f $(swiftBuild $@ --show-bin-path)/*.so "${LIBRARY_OUTPUT_DIRECTORY}"
 fi
